97943de849810dd97b05b405d054c4f5a2c63323
mccor2000 <roccm0200@gmail.com>
2020-10-15 14:37:42 +0700

    init: add mongo datasource & config

---
+++ src/datasources/mongo.datasource.ts
@@ -0,0 +1,29 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+  name: 'mongo',
+  connector: 'mongodb',
+  url: process.env.MONGODB_URL,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
+@lifeCycleObserver('datasource')
+export class MongoDataSource
+  extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'mongo';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mongo', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
