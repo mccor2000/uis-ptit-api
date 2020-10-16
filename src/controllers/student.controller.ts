7709d52e377ab47e59c53b5d8c15d8249afb9a99
mccor2000 <roccm0200@gmail.com>
2020-10-16 13:15:26 +0700

    remove: remove unused api endpoints such as post, put, pathc,..

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -6,16 +6,7 @@ import {
   repository,
   Where,
 } from '@loopback/repository';
-import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-} from '@loopback/rest';
+import {param, get, getModelSchemaRef} from '@loopback/rest';
 import {Student} from '../models';
 import {StudentRepository} from '../repositories';
 
@@ -24,30 +15,6 @@ export class StudentController {
     @repository(StudentRepository)
     public studentRepository: StudentRepository,
   ) {}
-
-  @post('/students', {
-    responses: {
-      '200': {
-        description: 'Student model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Student)}},
-      },
-    },
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Student, {
-            title: 'NewStudent',
-            exclude: ['_id'],
-          }),
-        },
-      },
-    })
-    student: Omit<Student, '_id'>,
-  ): Promise<Student> {
-    return this.studentRepository.create(student);
-  }
 
   @get('/students/count', {
     responses: {
@@ -82,28 +49,6 @@ export class StudentController {
     return this.studentRepository.find(filter);
   }
 
-  @patch('/students', {
-    responses: {
-      '200': {
-        description: 'Student PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Student, {partial: true}),
-        },
-      },
-    })
-    student: Student,
-    @param.where(Student) where?: Where<Student>,
-  ): Promise<Count> {
-    return this.studentRepository.updateAll(student, where);
-  }
-
   @get('/students/{id}', {
     responses: {
       '200': {
@@ -122,51 +67,5 @@ export class StudentController {
     filter?: FilterExcludingWhere<Student>,
   ): Promise<Student> {
     return this.studentRepository.findById(id, filter);
-  }
-
-  @patch('/students/{id}', {
-    responses: {
-      '204': {
-        description: 'Student PATCH success',
-      },
-    },
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Student, {partial: true}),
-        },
-      },
-    })
-    student: Student,
-  ): Promise<void> {
-    await this.studentRepository.updateById(id, student);
-  }
-
-  @put('/students/{id}', {
-    responses: {
-      '204': {
-        description: 'Student PUT success',
-      },
-    },
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() student: Student,
-  ): Promise<void> {
-    await this.studentRepository.replaceById(id, student);
-  }
-
-  @del('/students/{id}', {
-    responses: {
-      '204': {
-        description: 'Student DELETE success',
-      },
-    },
-  })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
-    await this.studentRepository.deleteById(id);
   }
 }
